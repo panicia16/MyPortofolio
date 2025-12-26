7c761ca47cba360c00ed6e3bcda7ca11423c5c72
panicia16 <panicia535230059@stu.untar.ac.id>
2025-12-26 20:31:55 +0700

    membuat section teknikal skill

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,12 @@ function App() {
           <q>Sedang berproses, terus belajar, dan tidak berhenti</q>
         </div>
 
-        {/* Tentang Saya */}
-        <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Panicia</h1>
-        <p className="textbase/loose mb-6 opacity-50 text-justify">
-          Saya adalah mahasiswa Teknik Informatika Universitas Tarumanagara dengan konsentrasi Data Mining. 
-          Saya memiliki ketertarikan pada analisis data, pengembangan web, serta pengujian perangkat lunak. 
-          Saya memiliki pengalaman dalam mengolah data menjadi visualisasi yang informatif, menyusun laporan 
-          analisis secara sistematis, dan mendukung proses pengembangan fitur. Selain itu, saya turut melakukan 
-          User Acceptance Testing (UAT) untuk memastikan kualitas, kelayakan, dan kelancaran penggunaan sistem. 
-          Saya terbuka untuk mempelajari hal baru, cepat beradaptasi, serta mampu bekerja dengan tim. 
-          Ke depannya, saya berkomitmen untuk mengembangkan karier di bidang teknologi, khususnya pada data science, 
-          web development, dan quality assurance.
+        {/* Intro */}
+        <h1 className="text-5xl/tight font-bold mb-3">Hi, Saya Panicia</h1>
+        <p className="text-base/loose mb-6 opacity-50 text-justify">
+          Memiliki minat pada Data Analysis, Data Science, Data Engineer, Backend, Frontend,
+          FullStack Development, dan Quality Assurance, serta terus belajaruntuk meningkatkan
+          skill dan pengalaman.
         </p>
 
         {/* Button Download CV dan Lihat Proyek */}
@@ -38,6 +33,133 @@ function App() {
 
       {/* Foto Profil */}
       <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
+    </div>
+
+    {/* Tentang */}
+    <div className="tentang mt-32 py-10">
+      <div className="xl;w-2/3 lg:w-3/4 wf mx-auto p-7 bg-zinc-800 rounded-lg">
+        <p className="text-base/loose mb-10">
+          Saya adalah mahasiswa Teknik Informatika Universitas Tarumanagara dengan konsentrasi Data Mining. 
+          Saya memiliki ketertarikan pada analisis data, pengembangan web, serta pengujian perangkat lunak. 
+          Saya memiliki pengalaman dalam mengolah data menjadi visualisasi yang informatif, menyusun laporan 
+          analisis secara sistematis, dan mendukung proses pengembangan fitur. Selain itu, saya turut melakukan 
+          User Acceptance Testing (UAT) untuk memastikan kualitas, kelayakan, dan kelancaran penggunaan sistem. 
+          Saya terbuka untuk mempelajari hal baru, cepat beradaptasi, serta mampu bekerja dengan tim. 
+          Ke depannya, saya berkomitmen untuk mengembangkan karier di bidang teknologi, khususnya pada data science, 
+          web development, dan quality assurance.
+        </p>
+
+        {/* Pendidikan */}
+        <div className="mt-6">
+          <h1 className="font-large font-bold">PENDIDIKAN</h1>
+
+          <div className="flex items-start gap-4">
+            <img src="public\assets\untar_logo.png" alt="logo untar"className="w-15 object-contain"/>
+          <div className="">
+            <p className="text-sm opacity-80">
+              <span className="font-medium">Universitas Tarumanagara</span> - Teknik Informatika
+            </p>
+            <p className="text-xs">3.81 / 4.00</p>
+            <p className="text-xs opacity-60">2023 – Sekarang</p>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      {/* SKILL TEKNIKAL */}
+      <div className="tools mt-32 px-4">
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-12 text-center">Skill Teknikal</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+          
+          {/* PEMROGRAMAN DAN FRAMEWORK */}
+          <div className="bg-zinc-800 p-6 rounded-xl">
+            <h2 className="text-xl sm:text-2xl font-bold mb-6 text-center">Pemrograman & Framework</h2>
+            <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-4 lg:grid-cols-5 gap-6 place-items-center">
+              {[
+                ["python.png", "Python"],
+                ["SQL.png", "SQL"],
+                ["js.png", "JavaScript"],
+                ["html.png", "HTML"],
+                ["CSS.png", "CSS"],
+                ["bootstrap.png", "Bootstrap"],
+                ["nodejs.png", "Node.js"],
+                ["php.png", "PHP"],
+                ["laravel.png", "Laravel"],
+                ["restAPI.png", "REST API"],
+                ["JSON.png", "JSON"],
+                ["tailwind.png", "Tailwind CSS"],
+                ["reactjs.png", "ReactJS"],
+              ].map(([img, name]) => (
+              <div key={name} className="group flex flex-col items-center w-16 h-16">
+                <img src={`/assets/tools/programming_and_framework/${img}`} alt={name} className="w-12 sm:w-14 bg-white p-2 rounded-md transition-transform duration-300 group-hover:scale-110"/>
+                <span className="mt-2 inset-0 flex items-center justify-center text-[10px] font-semibold text-zinc-900 bg-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center px-1 rounded-md z-30">{name}</span>
+              </div>
+              ))}
+            </div>
+          </div>
+          
+          {/* TOOLS & SOFTWARE */}
+          <div className="bg-zinc-800 p-6 rounded-xl">
+            <h2 className="text-xl sm:text-2xl font-bold mb-6 text-center">Alat & Software</h2>
+            <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-4 lg:grid-cols-5 gap-6 place-items-center">
+              {[
+                ["canva.png", "Canva"],
+                ["colab.png", "Google Colab"],
+                ["draw.io.png", "Draw.io"],
+                ["excel.jpg", "Microsoft Excel"],
+                ["figma.png", "Figma"],
+                ["git.png", "Git"],
+                ["github.png", "GitHub"],
+                ["gns3.jpg", "GNS3"],
+                ["google_workspace.png", "Google Workspace"],
+                ["jira.png", "Jira"],
+                ["jupyter.png", "Jupyter Notebook"],
+                ["laragon.png", "Laragon"],
+                ["linux.png", "Linux"],
+                ["looker.png", "Looker Studio"],
+                ["oracle_sql_dev.png", "Oracle SQL Developer"],
+                ["thunder.png", "Thunder Client"],
+                ["virtualbox.png", "VirtualBox"],
+                ["vscode.png", "Visual Studio Code"],
+                ["xampp.png", "XAMPP"],
+              ].map(([img, name]) => (
+            <div key={name} className="group flex flex-col items-center w-16 h-16">
+              <img src={`/assets/tools/tools_and_software/${img}`} alt={name} className="w-12 sm:w-14 bg-white p-2 rounded-md transition-transform duration-300 group-hover:scale-110"/>
+              <span className="mt-2 inset-0 flex items-center justify-center text-[10px] font-semibold text-zinc-900 bg-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center px-1 rounded-md z-30">{name}</span>
+              </div>
+              ))}
+            </div>
+          </div>
+
+          {/* DATABASE */}
+          <div className="bg-zinc-800 p-6 rounded-xl md:col-span-2 lg:col-span-1">
+            <h2 className="text-xl sm:text-2xl font-bold mb-6 text-center">Database</h2>
+            <div className="grid grid-cols-3 gap-6 place-items-center">
+              {[
+                ["MongoDB.png", "MongoDB"],
+                ["MySQL.png", "MySQL"],
+                ["postgreSQL.png", "PostgreSQL"],
+              ].map(([img, name]) => (
+            <div key={name} className="group flex flex-col items-center w-16 h-16">
+              <img src={`/assets/tools/database/${img}`} alt={name} className="w-12 sm:w-14 bg-white p-2 rounded-md transition-transform duration-300 group-hover:scale-110"/>
+              <span className="mt-2 inset-0 flex items-center justify-center text-[10px] font-semibold text-zinc-900 bg-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center px-1 rounded-md z-30">{name}</span>
+            </div> 
+              ))}
+            </div>
+            </div>
+        </div>
+      </div>
+
+      {/* PROYEK*/}
+      <div className="proyek mt-32 px-4">
+        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50">Berikut ini beberapa proyek yang telah saya buat.</p>
+        <div className="proyek-box">
+          
+        </div>
+      </div>
+
+
     </div>
     </>
   );
--- src/data.js
+++ src/data.js
@@ -6,17 +6,17 @@ const Image = {
 
 export default Image;
 
-import Tools1 from "/assets/tools/vscode.png";
-import Tools2 from "/assets/tools/reactjs.png";
-import Tools3 from "/assets/tools/nextjs.png";
-import Tools4 from "/assets/tools/tailwind.png";
-import Tools5 from "/assets/tools/bootstrap.png";
-import Tools6 from "/assets/tools/js.png";
-import Tools7 from "/assets/tools/nodejs.png";
-import Tools8 from "/assets/tools/github.png";
-import Tools9 from "/assets/tools/ai.png";
-import Tools10 from "/assets/tools/canva.png";
-import Tools11 from "/assets/tools/figma.png";
+import Tools1 from "/assets/tools/tools_and_software/vscode.png";
+// import Tools2 from "/assets/tools/reactjs.png";
+// import Tools3 from "/assets/tools/nextjs.png";
+// import Tools4 from "/assets/tools/tailwind.png";
+// import Tools5 from "/assets/tools/bootstrap.png";
+// import Tools6 from "/assets/tools/js.png";
+// import Tools7 from "/assets/tools/nodejs.png";
+// import Tools8 from "/assets/tools/github.png";
+// import Tools9 from "/assets/tools/ai.png";
+// import Tools10 from "/assets/tools/canva.png";
+// import Tools11 from "/assets/tools/figma.png";
 
 export const listTools = [
   {
@@ -26,76 +26,76 @@ export const listTools = [
     ket: "Code Editor",
     dad: "100",
   },
-  {
-    id: 2,
-    gambar: Tools2,
-    nama: "React JS",
-    ket: "Framework",
-    dad: "200",
-  },
-  {
-    id: 3,
-    gambar: Tools3,
-    nama: "Next JS",
-    ket: "Framework",
-    dad: "300",
-  },
-  {
-    id: 4,
-    gambar: Tools4,
-    nama: "Tailwind CSS",
-    ket: "Framework",
-    dad: "400",
-  },
-  {
-    id: 5,
-    gambar: Tools5,
-    nama: "Bootstrap",
-    ket: "Framework",
-    dad: "500",
-  },
-  {
-    id: 6,
-    gambar: Tools6,
-    nama: "Javascript",
-    ket: "Language",
-    dad: "600",
-  },
-  {
-    id: 7,
-    gambar: Tools7,
-    nama: "Node JS",
-    ket: "Javascript Runtime",
-    dad: "700",
-  },
-  {
-    id: 8,
-    gambar: Tools8,
-    nama: "Github",
-    ket: "Repository",
-    dad: "800",
-  },
-  {
-    id: 9,
-    gambar: Tools9,
-    nama: "Adobe Illustrator",
-    ket: "Design App",
-    dad: "900",
-  },
-  {
-    id: 10,
-    gambar: Tools10,
-    nama: "Canva",
-    ket: "Design App",
-    dad: "1000",
-  },
-  {
-    id: 11,
-    gambar: Tools11,
-    nama: "Figma",
-    ket: "Design App",
-    dad: "1100",
-  },
+  // {
+  //   id: 2,
+  //   gambar: Tools2,
+  //   nama: "React JS",
+  //   ket: "Framework",
+  //   dad: "200",
+  // },
+  // {
+  //   id: 3,
+  //   gambar: Tools3,
+  //   nama: "Next JS",
+  //   ket: "Framework",
+  //   dad: "300",
+  // },
+  // {
+  //   id: 4,
+  //   gambar: Tools4,
+  //   nama: "Tailwind CSS",
+  //   ket: "Framework",
+  //   dad: "400",
+  // },
+  // {
+  //   id: 5,
+  //   gambar: Tools5,
+  //   nama: "Bootstrap",
+  //   ket: "Framework",
+  //   dad: "500",
+  // },
+  // {
+  //   id: 6,
+  //   gambar: Tools6,
+  //   nama: "Javascript",
+  //   ket: "Language",
+  //   dad: "600",
+  // },
+  // {
+  //   id: 7,
+  //   gambar: Tools7,
+  //   nama: "Node JS",
+  //   ket: "Javascript Runtime",
+  //   dad: "700",
+  // },
+  // {
+  //   id: 8,
+  //   gambar: Tools8,
+  //   nama: "Github",
+  //   ket: "Repository",
+  //   dad: "800",
+  // },
+  // {
+  //   id: 9,
+  //   gambar: Tools9,
+  //   nama: "Adobe Illustrator",
+  //   ket: "Design App",
+  //   dad: "900",
+  // },
+  // {
+  //   id: 10,
+  //   gambar: Tools10,
+  //   nama: "Canva",
+  //   ket: "Design App",
+  //   dad: "1000",
+  // },
+  // {
+  //   id: 11,
+  //   gambar: Tools11,
+  //   nama: "Figma",
+  //   ket: "Design App",
+  //   dad: "1100",
+  // },
 ];
 
 import Proyek1 from "/assets/proyek/proyek1.webp";
